bfbcf7fc47c8fb1cdc6f9a607a796885476e4263
Emery Tate <tateejt@gmail.com>
2019-10-24 05:44:10 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -14,4 +14,12 @@ class Meal
   def self.all 
     @@all 
   end
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+end
+
+def meals
+  Meal.all.select {|meal| meal.customer == self}
+end
 end
